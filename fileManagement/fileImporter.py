2df6beba77ad06fe1a8443fd98dd4b8937676b1e
Aimee <37088502+Aimzj@users.noreply.github.com>
2018-04-29 15:45:34 +0200

    Update fileImporter.py
    No GUI
    same functionality

--- fileManagement/fileImporter.py
+++ fileManagement/fileImporter.py
@@ -1,38 +1,13 @@
-import pyforms
-from pyforms            import BaseWidget
-from pyforms.controls   import ControlButton
-from pyforms.controls   import ControlFile
-
 import os
 
-import csv
-
-class fileImporter(BaseWidget):
+class fileImporter():
 
     def __init__(self):
-        super(fileImporter,self).__init__('Import File')
+        pass
 
-        #Definitions of the form fields
-        self._controlFile = ControlFile('Import File')
-        self._buttonCancel = ControlButton('Cancel')
-        self._buttonDone = ControlButton('Done')
 
-        #Define the button actions
-        self._buttonCancel.value = self.__buttonCancelAction
-        self._buttonDone.value = self.__buttonDoneAction
 
-        #Define the organisation of the forms
-        self.formset = [' ',' ',(' ','_controlFile',' '),'=',' ',(' ',' ',' ',' ','_buttonCancel','_buttonDone',' '),' ']
-
-    #counting the columns
-    def __countCols(self, filePath):
-        self._file = open(filePath)
-        self._file.readline()
-        self._list = self._file.readline().split()
-        self._file.close()
-        return len(self._list)
-    
-    #counting number of lines in file
+        # counting number of lines in file
     def __fileLength(self, filePath):
         self._file = open(filePath)
         for i, line in enumerate(self._file):
@@ -40,8 +15,8 @@ class fileImporter(BaseWidget):
         self._file.close()
         return i + 1
 
-    #generating a list of lists from data in file
-    #each sublist is a row in the file
+        # generating a list of lists from data in file
+        # each sublist is a row in the file
     def __genDataList(self, filePath, length):
         self._file = open(filePath)
         dataList = []
@@ -50,44 +25,58 @@ class fileImporter(BaseWidget):
             dataList.append(tempList)
         self._file.close()
         return dataList
-    
-    def __buttonCancelAction(self):
-        pass
 
-    def __buttonDoneAction(self):        
-        #extracting the data file name(incl. extension) from the path
-        self._filePath = self._controlFile.value
-        self._fileName = os.path.basename(self._filePath)
-        #determining the file type
-        self._isPca = self._fileName.find(".pca.evec")
+    def ValidateFile(self, fileType, filePath):
+        # extracting file name with extension
+        self._fileName = os.path.basename(filePath)
+
+        # determining the file type
+        self._isPca = self._fileName.find(".evec")
         self._isAdmix = self._fileName.find(".Q.")
         self._isFam = self._fileName.find(".fam")
         self._isPheno = self._fileName.find(".phe")
         if self._isPca > -1:
-            self._fileType = 'PCA'
+            if fileType == 'PCA':
+                self._fileType = fileType
+                return True
+            else:
+                return False
+
         elif self._isAdmix > -1:
-            self._fileType = 'Admixture'
+            if fileType == 'Admixture':
+                self._fileType = fileType
+                return True
+            else:
+                return False
+
         elif self._isFam > -1:
-            self._fileType = 'Fam'
+            if fileType == 'Fam':
+                self._fileType = fileType
+                return True
+            else:
+                return False
+
         elif self._isPheno > -1:
-            self._fileType = 'Phenotype'
+            if fileType == 'Phenotype':
+                self._fileType = fileType
+                return True
+            else:
+                return False
+
         else:
-            raise ValueError('Invalid file type selected')
-        
-        #calculating file length
-        self._length = self.__fileLength(self._filePath)
+            return False
 
-        #generating list of data from file
-        self._dataList = self.__genDataList(self._filePath, self._length)
+    def ImportFile(self, filePath):
+        # calculating file length
+        self._length = self.__fileLength(self, filePath)
 
-        #defining dictionary of data
+        # generating list of data from file
+        self._dataList = self.__genDataList(self, filePath, self._length)
+
+        # defining dictionary of data
         self._dataDictionary = {
             'fileName': self._fileName,
             'fileType': self._fileType,
             'data': self._dataList
-            }
+        }
         return self._dataDictionary
-    
-#Execute the application
-if __name__ == "__main__": pyforms.start_app(fileImporter, geometry=(500,300,400,100))
-#setting geometry: first two = x and y pos, second two = x and y size
