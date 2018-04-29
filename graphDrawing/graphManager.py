fa00672514f34f5f2ce2c19320258be2eaf6999d
KenanKarav <k.karavoussanos@gmail.com>
2018-04-29 11:59:31 +0200

    removes pyforms code, adds matplot embedding code in wxPython

--- graphDrawing/graphManager.py
+++ graphDrawing/graphManager.py
@@ -1,1 +1,68 @@
 
+import wx
+import wx.lib.mixins.inspection as wit
+
+if 'phoenix' in wx.PlatformInfo:
+    import wx.lib.agw.aui as aui
+else:
+    import wx.aui as aui
+
+import matplotlib as mpl
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+from matplotlib.backends.backend_wxagg import NavigationToolbar2WxAgg as NavigationToolbar
+
+
+class Plot(wx.Panel):
+    def __init__(self, parent, id=-1, dpi=None, **kwargs):
+        wx.Panel.__init__(self, parent, id=id, **kwargs)
+        self.figure = mpl.figure.Figure(dpi=dpi, figsize=(2, 2))
+        self.canvas = FigureCanvas(self, -1, self.figure)
+        self.toolbar = NavigationToolbar(self.canvas)
+        self.toolbar.Realize()
+
+        # adding components
+        self.menubar = wx.MenuBar( 0 )
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        sizer.Add(self.canvas, 1, wx.EXPAND)
+        sizer.Add(self.toolbar, 0, wx.LEFT | wx.EXPAND)
+        self.SetSizer(sizer)
+
+
+class PlotNotebook(wx.Panel):
+    def __init__(self, parent, id=-1):
+        wx.Panel.__init__(self, parent, id=id)
+        self.nb = aui.AuiNotebook(self)
+        sizer = wx.BoxSizer()
+        sizer.Add(self.nb, 1, wx.EXPAND)
+        self.SetSizer(sizer)
+
+    def add(self, name="plot"):
+        page = Plot(self.nb)
+        self.nb.AddPage(page, name)
+        return page.figure
+
+class Menu(wx.Frame):
+    def __init__(self,parent, id=-1):
+        frame = wx.Frame(self, -1, 'ReGenesis')
+        self.MenuBar = wx.MenuBar()
+
+def render():
+    # alternatively you could use
+    #app = wx.App()
+    # InspectableApp is a great debug tool, see:
+    # http://wiki.wxpython.org/Widget%20Inspection%20Tool
+    app = wit.InspectableApp()
+
+    frame = wx.Frame(None, -1 , "ReGenesis")
+    plotter = PlotNotebook(frame)
+    axes1 = plotter.add('figure 1').gca()
+    axes1.plot([1, 2, 3], [2, 1, 4])
+    axes2 = plotter.add('figure 2').gca()
+    axes2.plot([1, 2, 3, 4, 5], [2, 1, 4, 2, 3])
+    frame.Show()
+    app.MainLoop()
+
+if __name__ == "__main__":
+    render()
