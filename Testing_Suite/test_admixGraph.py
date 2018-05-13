5a737d94f155ff8f3c6780de4998983baaf39115
Dhevan Lau <1433596@students.wits.ac.za>
2018-05-13 11:52:10 +0200

    Updated admix graph test module.

--- Testing_Suite/test_admixGraph.py
+++ Testing_Suite/test_admixGraph.py
@@ -4,19 +4,30 @@ from graphDrawing.graphs.admixtureGraph.admixtureGraph import admixtureGraph
 class TestAdmixGraph(unittest.TestCase):
 
     def setUp(self):
+        # Test Graph with Pheno Fata
         testDict = {'AdmixFile' : None, 'AdmixIDs' : ['ID1', 'ID2', 'ID3'], 'Ancestry1' : [0.33, 0.48, 0.58], 'Ancestry2' : [0.60, 0.22, 0.24], 'Ancestry3' : [0.07, 0.30, 0.18]
                     , 'PhenoFile' : None, 'PhenoIDs' : ['ID1', 'ID2', 'ID3'], 'PhenoColumn' : ['Green', 'Blue', 'Green']}
         self.myGraph = admixtureGraph(testDict)
 
+        # Test Graph without pheno Data
+        testDict2 = {'AdmixFile': None, 'AdmixIDs': ['ID1', 'ID2', 'ID3'], 'Ancestry1': [0.33, 0.48, 0.58],
+                    'Ancestry2': [0.60, 0.22, 0.24], 'Ancestry3': [0.07, 0.30, 0.18]}
+        self.myGraph2 = admixtureGraph(testDict2)
+
     def test_GetAdmixIDs(self):
         self.assertEqual(self.myGraph.getAdmixIDs(),  ['ID1', 'ID2', 'ID3'])
+        self.assertEqual(self.myGraph2.getAdmixIDs(), ['ID1', 'ID2', 'ID3'])
 
     def test_GetAncestry(self):
         tempDict = self.myGraph.getAncestryDict()
         self.assertEqual(tempDict, {'Ancestry1' : [0.33, 0.48, 0.58], 'Ancestry2' : [0.60, 0.22, 0.24], 'Ancestry3' : [0.07, 0.30, 0.18]})
 
+        tempDict = self.myGraph2.getAncestryDict()
+        self.assertEqual(tempDict, {'Ancestry1': [0.33, 0.48, 0.58], 'Ancestry2': [0.60, 0.22, 0.24], 'Ancestry3': [0.07, 0.30, 0.18]})
+
     def test_GetNumAncestries(self):
         self.assertEqual(self.myGraph.getNumAncestries(), 3)
+        self.assertEqual(self.myGraph2.getNumAncestries(), 3)
 
     def test_GetPhenoDict(self):
         tempDict = self.myGraph.getPhenoDict()
@@ -27,7 +38,22 @@ class TestAdmixGraph(unittest.TestCase):
 
     def test_GenGroupDictionary(self):
         tempDict = self.myGraph.genDataDictionary()
-        self.assertEqual(self.myGraph.genDataDictionary(),{'Green':{'Ancestry1':[0.33, 0.58], 'Ancestry2':[0.60,0.24], 'Ancestry3':[0.07,0.18]} , 'Blue' : {'Ancestry1':[0.48],'Ancestry2':[0.22],'Ancestry3':[0.30]}})
+        self.assertEqual(tempDict,{'Green':{'Ancestry1':[0.33, 0.58], 'Ancestry2':[0.60,0.24], 'Ancestry3':[0.07,0.18]}
+            , 'Blue' : {'Ancestry1':[0.48],'Ancestry2':[0.22],'Ancestry3':[0.30]}})
+
+        tempDict = self.myGraph2.genDataDictionary()
+        self.assertEqual(tempDict, {'ungrouped': {'Ancestry1': [0.33, 0.48, 0.58], 'Ancestry2': [0.60, 0.22, 0.24],
+                                                  'Ancestry3': [0.07, 0.30, 0.18]}})
+
+
+    def test_GetSaveFileData(self):
+        tempDict = self.myGraph.getSaveFileData()
+        self.assertEqual(tempDict, {'AdmixIDs' : ['ID1', 'ID2', 'ID3'], 'Ancestry1' : [0.33, 0.48, 0.58], 'Ancestry2' : [0.60, 0.22, 0.24], 'Ancestry3' : [0.07, 0.30, 0.18]
+                    , 'PhenoIDs' : ['ID1', 'ID2', 'ID3'], 'PhenoColumn' : ['Green', 'Blue', 'Green']})
+
+    def test_GetGraphType(self):
+        self.assertEqual(self.myGraph.getGraphType(), 'admix')
+        self.assertEqual(self.myGraph2.getGraphType(), 'admix')
 
 
 if __name__ == '__main__':
